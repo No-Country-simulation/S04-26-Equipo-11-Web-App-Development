1ffc63c7db0850d85e9019fd395b0cb99d188ce0
francocasafus22 <francocasafus55@gmail.com>
2026-05-06 00:42:42 -0300

    add logo

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { Button } from "./components/ui/button";
 import { ModeToggle } from "./components/ui/themeToggle";
+import Logo from "./components/logo";
 
 export default function HomePage() {
 
     // TODO: Add landing page with some info about OpsCore and links to login and register pages
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen gap-3">
+        <div className="flex flex-col justify-center items-center h-screen gap-3">            
             
+            <Logo className={"mb-0 w-38"}/>
             <h1 className="text-8xl font-bold">OpsCore</h1>
             <p>Welcome to the OpsCore system!</p>
             <ModeToggle/>
