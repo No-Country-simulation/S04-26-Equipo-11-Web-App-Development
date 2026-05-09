33721f01a39a18e9b82fc9b0debfb69bb7ed6918
Facundo Mosquera <facundomosquerabus@gmail.com>
2026-05-09 17:29:31 -0300

    creado modelo base JWTUtil

---
+++ backend/src/main/java/com/equipo11/opscore/model/auth/JWTUtil.java
@@ -0,0 +1,67 @@
+package com.equipo11.opscore.model.auth;
+
+import com.equipo11.opscore.model.Usuario;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(Usuario usuario) {
+        Date creationDate = new Date();
+        return Jwts.builder()
+                .subject(usuario.getId().toString())
+                .issuedAt(creationDate)
+                .expiration(new Date(creationDate.getTime() + jwtExpirationMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getUserFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (SecurityException e) {
+            System.out.println("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("JWT token is expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("JWT token is unsupported: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty: " + e.getMessage());
+        }
+        return false;
+    }
+}
