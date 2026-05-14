96a79b8c0dc23356682e0f6318ffa3eb64a44346
francocasafus22 <francocasafus55@gmail.com>
2026-05-14 19:49:21 -0300

    add: create incident button

--- frontend/src/OperatorPage.tsx
+++ frontend/src/OperatorPage.tsx
@@ -1,3 +1,5 @@
+import { Plus } from "lucide-react";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 
 export default function OperatorPage() {
   return (
@@ -10,8 +12,45 @@ export default function OperatorPage() {
 
         <p className="text-muted-foreground">
           Reportá incidentes y seguí el estado de tus solicitudes.
-        </p>
+        </p>        
       </div>
+
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Card className="bg-primary text-primary-foreground transition hover:scale-[1.02] hover:cursor-pointer">
+            <CardContent className="flex h-full min-h-55 flex-col items-center justify-center gap-4 p-6">
+
+                <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary-foreground/10">
+                <Plus className="h-10 w-10" />
+                </div>
+
+                <div className="text-center">
+                <h3 className="text-xl font-semibold">
+                    Crear incidente
+                </h3>
+
+                <p className="text-sm text-primary-foreground/80">
+                    Reportá rápidamente una nueva falla o problema.
+                </p>
+                </div>
+
+            </CardContent>
+        </Card>
+        <Card>
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+            </CardHeader> 
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+        </Card>         
+      </div>
+
+        
 
     </div>
   );
